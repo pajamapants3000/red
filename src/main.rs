5ddea21231dc0eaf1a933efb8978dbd1299dd41e
Tommy Lincoln <pajamapants3000@gmail.com>
2016-10-25 06:10:12 -0700

    get opchar index in cmd_input

--- src/main.rs
+++ src/main.rs
@@ -23,17 +23,21 @@
 
 use std::env;
 use std::fs::{File, OpenOptions};
+use std::str::Chars;
 use::std::io::prelude::*;
 // Use LineWriter instead of, or in addition to, BufWriter?
-use::std::io::{self,BufReader,BufWriter,Error};
+use::std::io::{self,BufReader,BufWriter};
 
 // }}}
+
+// Crate Attributes {{{
+//}}}
 
 // *** Constants *** {{{
 // Define messages
 // Some of these may be removed if builtin error descriptions work
-const S_FOPEN: &'static str = "successfully opened file!";
-//const E_FOPEN: &'static str = "unable to open file";
+const S_FOPEN_MSG: &'static str = "successfully opened file!";
+//const E_FOPEN_MSG: &'static str = "unable to open file";
 
 // Additional string constants
 const LINE_CONT: &'static str = "\\\n";
@@ -60,6 +64,11 @@ struct Command<'a> {
     parameters: &'a str,
 }
 
+enum RedError {
+    FileOpen,
+    FileClose,
+}
+
 // ^^^ Data Structures ^^^ }}}
 
 // Main {{{
@@ -75,11 +84,12 @@ fn main() {
     match file_opener( file_name, file_mode ) {
         Ok(f) => {
             file_opened = f;
-            println!( "{}", S_FOPEN );
+            println!( "{}", S_FOPEN_MSG );
             println!( "our file is: {:?}", file_opened );
         },
         Err(e) => {
             println!( "error: {}", e );
+            std::process::exit( error_code( RedError::FileOpen ) as i32 );
 
         },
     };
@@ -90,6 +100,7 @@ fn main() {
     let mut cli_writer = BufWriter::new(io::stdout());
     let mut cmd_input = String::new();
     let mut prompt = PROMPT.to_string();
+    let mut user_quit: bool = false;
 
     cli_writer.write(format!("{}", prompt).as_bytes()).unwrap();
     cli_writer.flush().unwrap();
@@ -101,7 +112,8 @@ fn main() {
             prompt = PROMPT_CONT.to_string();
         } else {
             {                                            // Execute command {{{
-                let command: Command = parse_command( &cmd_input );
+                let command: Command =
+                        parse_command( &cmd_input, &file_opened );
                 // just some test output
                 cli_writer.write(command.parameters.as_bytes()).unwrap();
                 cli_writer.write(command.address_initial.to_string()
@@ -114,6 +126,11 @@ fn main() {
                                  .as_bytes()).unwrap();
                 cli_writer.write(b"\n").unwrap();
 
+                match command.operation {
+                    'q' => user_quit = true,
+                    _ => ()
+                }
+
             }                                           // Done executing }}}
             // ready for a new command
             cmd_input.clear();
@@ -121,12 +138,16 @@ fn main() {
             prompt = PROMPT.to_string();
         }
 
+        if user_quit { break }
+
         // prompt for the next round
         cli_writer.write(format!("{}", prompt).as_bytes()).unwrap();
         // put it all to the screen
         cli_writer.flush().unwrap();
     }
     //}}}
+    
+    std::process::exit( error_code( RedError::FileClose ) as i32 );
     
 }
 //}}}
@@ -136,8 +157,8 @@ fn main() {
 /// Uses global definitions of mode flags in this file
 ///
 /// Returns direct result of call to OpenOptions::new()
-/// This is of type Result<File, Error>
-fn file_opener( name: &str, mode: FileMode ) -> Result<File, Error> {
+/// This is of type Result<File, io::Error>
+fn file_opener( name: &str, mode: FileMode ) -> Result<File, io::Error> {
 
     // let's introduce OpenOptions now, though we don't need it
     // until we introduce more functionality
@@ -154,12 +175,202 @@ fn file_opener( name: &str, mode: FileMode ) -> Result<File, Error> {
 
 /// Parses command-mode input {{{
 ///
-fn parse_command( _cmd_input: &str ) -> Command {
+fn parse_command<'a>( _cmd_input: &'a str, file_opened: &File ) -> Command<'a> {
+    // MUST initialize?
+    let mut _address_initial: u32 = 1;
+    let mut _address_final: u32 = 1;
+    let mut _operation: char = 'p';
+    let mut _parameters: &str = _cmd_input;
+
+    let op_indx: usize;
+    op_indx = get_opchar_index( _cmd_input ).unwrap_or( 0usize );
+    _address_final = op_indx as u32;
+
+    match get_address( "", file_opened ) {
+        ( x, y ) => {
+            _address_initial = x;
+            _address_final = y;
+        }
+    }
+
     Command {
-        address_initial: 1,
-        address_final: 1,
-        operation: 'p',
-        parameters: _cmd_input,
+        address_initial: _address_initial,
+        address_final: _address_final,
+        operation: _operation,
+        parameters: _parameters,
     }
 }
-//]]]
+//}}}
+
+/// Identify address or address range {{{
+///
+#[allow(unused_variables)]
+fn get_address( address_string: &str, file_opened: &File ) -> ( u32, u32 ) {
+    // start with simple auto-return of ( 1u32, 2u32 )
+    ( 1u32, 2u32 )
+}
+//}}}
+
+/// Return error code for given error type {{{
+///
+fn error_code( _error: RedError ) -> u32 {
+    match _error {
+        RedError::FileOpen => 280,
+        RedError::FileClose => 281,
+    }
+}
+//}}}
+
+/// Find index of operation code in string
+///
+/// Parses full command and finds the index of the operation character;
+/// The trick here is that the operation character may appear any number
+/// of times as part of a regular expression used to specify an address
+/// range that matches;
+/// What this function does is simply locates the first alphabetic character
+/// that is not wrapped in either /.../ or ?...?
+///
+/// Trims white space on the left as well - does not count these characters
+///
+/// # Examples
+///
+/// No address given
+/// ```rust
+/// let _in: &str = "e myfile.txt";
+/// assert_eq!( get_opchar_index( _in ), 0 );
+/// ```
+///
+/// No address given, with spaces
+/// ```rust
+/// let _in: &str = "       e myfile.txt";
+/// assert_eq!( get_opchar_index( _in ), 0 );
+/// ```
+///
+/// No address given, with spaces and tabs
+/// ```rust
+/// let _in: &str = "  		  	e myfile.txt";
+/// assert_eq!( get_opchar_index( _in ), 0 );
+/// ```
+///
+/// Most basic address value types
+/// ```rust
+/// let _in: &str = ".a";
+/// assert_eq!( get_opchar_index( _in ), 1 );
+/// ```
+///
+/// ```rust
+/// let _in: &str = ".,.p";
+/// assert_eq!( get_opchar_index( _in ), 3 );
+/// ```
+///
+/// Slightly more complicated
+/// ```rust
+/// let _in: &str = ".-2,.+2p";
+/// assert_eq!( get_opchar_index( _in ), 3 );
+/// ```
+///
+/// Regular expression match line search forward
+/// ```rust
+/// let _in: &str = "/^Beginning with.*$/;/.* at the end$/s_mytest_yourtest_g";
+/// assert_eq!( get_opchar_index( _in ), 37 );
+/// ```
+///
+/// Regular expression match line search forward with spaces and tabs
+/// ```rust
+/// let _in: &str =
+/// "		  	/^Beginning with.*$/;/.* at the end$/s_mytest_yourtest_g";
+/// assert_eq!( get_opchar_index( _in ), 37 );
+/// ```
+///
+/// Regular expression match line search backward
+/// ```rust
+/// let _in: &str = "?^Beginning with.*$?,?.* at the end$?s_mytest_yourtest_g";
+/// assert_eq!( get_opchar_index( _in ), 37 );
+/// ```
+///
+fn get_opchar_index( _cmd_input: &str ) -> Result<usize, RedError> {
+    let mut result: i32 = -1;
+
+    struct Interpret {
+        index: u16,         // index of character in string
+        pattern_char: char, // stores '/' or '?' while reading pattern
+        escape: bool,       // last character read was '\'
+        expect: bool,       // we expect next char to be ',', ';', or alpha
+    }
+    let mut current_state: Interpret = Interpret {
+        index: 0u16,
+        pattern_char: '\0',
+        escape: false,
+        expect: false,
+    };
+
+    let mut ch_iter: Chars = _cmd_input.trim_left().chars();
+
+    loop {
+        match ch_iter.next() {
+            None => break,
+            Some( x ) => {
+                if current_state.pattern_char == '\0' {
+                    if current_state.expect {
+                        match x {
+                            'a'...'z' | 'A'...'Z' => {
+                                result = current_state.index as i32;
+                                break;
+                            },
+                            ',' | ';' => {
+                                current_state.escape= false;
+                                current_state.expect = false
+                            },
+                            '\\' => current_state.escape = true,
+                            _ => break,
+                        }
+                    }
+                    match x {
+                        '/' | '?' => {
+                            if !current_state.escape {
+                                current_state.pattern_char = x;
+                            }
+                        }
+                        'a'...'z' | 'A'...'Z' => {
+                            result = current_state.index as i32;
+                            break;
+                        },
+                        '\\' => current_state.escape = true,
+                        _ => {
+                            current_state.escape= false;
+                            current_state.expect = false;
+                        },
+                    }
+                } else {
+                    match x {
+                        '/' => {
+                            if '/' == current_state.pattern_char {
+                                if !current_state.escape {
+                                    current_state.pattern_char = '\0';
+                                    current_state.expect = true;
+                                }
+                            }
+                        }
+                        '?' => {
+                            if '?' == current_state.pattern_char {
+                                if !current_state.escape {
+                                    current_state.pattern_char = '\0';
+                                    current_state.expect = true;
+                                }
+                            }
+                        }
+                        '\\' => current_state.escape = true,
+                        _ => {},
+                    }
+                }
+            }
+        }
+        current_state.index += 1;
+    }
+    if result < 0 {
+        Result::Err( RedError::FileClose )
+    } else {
+        println!("result {:?}", result);
+        Result::Ok( result as usize )   // result will be positive here
+    }
+}
