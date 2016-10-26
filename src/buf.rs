47e0a3908f0a98338fca2b37d64ac3ea9b998dee
Tommy Lincoln <pajamapants3000@gmail.com>
2016-10-26 05:01:07 -0700

    create buf, io modules

---
+++ src/buf.rs
@@ -0,0 +1,23 @@
+/*
+ * File   : buf.rs
+ * Purpose: read/write buffer during user interaction
+ * Program: red
+ * About  : command-line text editor
+ * Authors: Tommy Lincoln <pajamapants3000@gmail.com>
+ * License: MIT; See LICENSE!
+ * Notes  : Notes on successful compilation
+ * Created: 10/26/2016
+ */
+
+// *** Bring in to namespace *** {{{
+// ^^^ Bring in to namespace ^^^ }}}
+// *** Attributes *** {{{
+// ^^^ Attributes ^^^ }}}
+// *** Constants *** {{{
+// ^^^ Constants ^^^ }}}
+// *** Data Structures *** {{{
+// ^^^ Data Structures ^^^ }}}
+// *** Functions *** {{{
+// ^^^ Functions ^^^ }}}
+
+
---
+++ src/io.rs
@@ -0,0 +1,32 @@
+/*
+ * File   : io.rs
+ * Purpose: 
+ * Program: red
+ * About  : What does this program do?
+ * Authors: Tommy Lincoln <pajamapants3000@gmail.com>
+ * License: MIT; See LICENSE!
+ * Notes  : Notes on successful compilation
+ * Created: 10/26/2016
+ */
+
+// *** Bring in to namespace *** {{{
+// ^^^ Bring in to namespace ^^^ }}}
+// *** Attributes *** {{{
+// ^^^ Attributes ^^^ }}}
+// *** Constants *** {{{
+// ^^^ Constants ^^^ }}}
+// *** Data Structures *** {{{
+struct FileCoordinate {
+    line: usize,
+    col: usize,
+}
+
+// ^^^ Data Structures ^^^ }}}
+// *** Functions *** {{{
+/// Return next occurrence of regular expression
+regex_search( needle: &str, from: FileCoordinate ) -> FileCoordinate {
+}
+
+// ^^^ Functions ^^^ }}}
+
+
