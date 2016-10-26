63eaaea869e6ddc0b4f5088b246ecde2c0bd82c3
Tommy Lincoln <pajamapants3000@gmail.com>
2016-10-26 04:44:44 -0700

    move private parse fn tests into submod

--- src/parse.rs
+++ src/parse.rs
@@ -78,6 +78,8 @@ fn get_address( address_string: &str, file_opened: &File ) -> ( u32, u32 ) {
 ///
 /// # Examples
 ///
+/// All examples repeated in tests module below
+///
 /// No address given
 /// ```rust
 /// let _in: &str = "e myfile.txt";
@@ -132,7 +134,7 @@ fn get_address( address_string: &str, file_opened: &File ) -> ( u32, u32 ) {
 /// assert_eq!( get_opchar_index( _in ), 37 );
 /// ```
 ///
-pub fn get_opchar_index( _cmd_input: &str ) -> Result<usize, RedError> {
+fn get_opchar_index( _cmd_input: &str ) -> Result<usize, RedError> {
     let mut result: i32 = -1;
 
     struct Interpret {
@@ -217,4 +219,74 @@ pub fn get_opchar_index( _cmd_input: &str ) -> Result<usize, RedError> {
         println!("result {:?}", result);
         Result::Ok( result as usize )   // result will be positive here
     }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::get_opchar_index;
+
+// Tests for parse::get_opchar_index
+/// No address given
+#[test]
+fn get_opchar_index_test_1() {
+       let _in: &str = "e myfile.txt";
+       assert_eq!( get_opchar_index( _in ).unwrap_or(9999), 0 );
+}
+
+/// No address given, with spaces
+#[test]
+fn get_opchar_index_test_2() {
+    let _in: &str = "       e myfile.txt";
+    assert_eq!( get_opchar_index( _in ).unwrap_or(9999), 0 );
+}
+
+/// No address given, with spaces and tabs
+#[test]
+fn get_opchar_index_test_3() {
+    let _in: &str = "  		  	e myfile.txt";
+    assert_eq!( get_opchar_index( _in ).unwrap_or(9999), 0 );
+}
+
+/// Most basic address value types
+#[test]
+fn get_opchar_index_test_4() {
+    let _in: &str = ".a";
+    assert_eq!( get_opchar_index( _in ).unwrap_or(9999), 1 );
+}
+
+#[test]
+fn get_opchar_index_test_5() {
+    let _in: &str = ".,.p";
+    assert_eq!( get_opchar_index( _in ).unwrap_or(9999), 3 ); // test
+}
+
+/// Slightly more complicated
+#[test]
+fn get_opchar_index_test_6() {
+    let _in: &str = ".-2,.+2p";
+    assert_eq!( get_opchar_index( _in ).unwrap_or(9999), 7 ); //test
+}
+
+/// Regular expression match line search forward
+#[test]
+fn get_opchar_index_test_7() {
+    let _in: &str = "/^Beginning with.*$/;/.* at the end$/s_mytest_yourtest_g";
+    assert_eq!( get_opchar_index( _in ).unwrap_or(9999), 37 );
+}
+
+/// Regular expression match line search forward with spaces and tabs
+#[test]
+fn get_opchar_index_test_8() {
+    let _in: &str =
+    "		  	/^Beginning with.*$/;/.* at the end$/s_mytest_yourtest_g";
+    assert_eq!( get_opchar_index( _in ).unwrap_or(9999), 37 );
+}
+
+/// Regular expression match line search backward
+#[test]
+fn get_opchar_index_test_9() {
+    let _in: &str = "?^Beginning with.*$?,?.* at the end$?s_mytest_yourtest_g";
+    assert_eq!( get_opchar_index( _in ).unwrap_or(9999), 37 );
+}
+
 }
